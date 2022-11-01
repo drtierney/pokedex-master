725706f534430114ff44f4bce85fe5a72fc0d4d8
drtierney <d.r.tierney@hotmail.co.uk>
2022-11-01 19:59:38 +0000

    Renamed pokedex-minify.json to pokedex.json and added code to read json file

--- script.py
+++ script.py
@@ -1,2 +1,14 @@
-name = input("Please enter your name: ")
-print(f"Hello {name}! Welcome to the world of Pokemon!")
+import json
+import random
+
+print("Welcome to the world of Pokemon!")
+
+with open("pokedex.json", "r") as json_file:
+    pokedex = json.load(json_file)
+
+print(len(pokedex))
+print(pokedex[0])
+print("Picking 5 random Pokemon:")
+for i in range(5):
+    pokemon = random.choice(pokedex)
+    print(pokemon.get("no"), "-", pokemon.get("name").title(), ":", ",".join(pokemon.get("abilities")).title().replace("-", " "))
